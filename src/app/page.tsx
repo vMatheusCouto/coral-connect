b396d6281fab2e3a943092b44970b3e548247a8f
vMatheusCouto <ctheusg@hotmail.com>
2025-04-28 09:00:25 -0300

    feat: header start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,9 @@ import {
   SidebarSeparator,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
-import { Avatar } from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
 
 export default function Page() {
   const titles: string[] = ['Featured articles', 'Recent articles', 'All articles', 'Most viewed', 'Explore']
@@ -18,8 +20,18 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset className="overflow-hidden">
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 max-w[calc(100% - 256px)]">
+        <header className="flex h-16 shrink-0 items-center place-content-between gap-2 border-b px-4 max-w[calc(100% - 256px)]">
           <SidebarTrigger className="-ml-1" />
+          <div className="flex flex-row justify-center">
+            <Input className="max-w-8/12 rounded-r-none"/>
+            <Button variant="outline" size="icon" className="rounded-l-none border-l-0">
+              <Search />
+            </Button>
+          </div>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
         </header>
           {titles.map((title, j) => {
             return (
